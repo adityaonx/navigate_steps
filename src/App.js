25a60c2d6e2c185829d01fa2fc6c4b5864bd9a81
adityaonx <adityaonx@gmail.com>
2023-11-08 21:51:41 +0530

    v2. Added Open,Close button functionality to (show,hide) the Steps component using setIsOpen() using useState(). Also added fragment <> instead of <div> to hide div as parent element.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
   //useState is a hook, starts with "use", only call hooks at top of function
   //Also cant place hooks inside conditional statements
   const [step, setStep] = useState(1);
+  //saves the state for if open value is true or false, (visible or hidden)
+  const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
     if (step > 1) setStep((step) => step - 1);
   }
@@ -16,35 +18,45 @@ function App() {
     if (step < 3) setStep((step) => step + 1);
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <div className="message">
-        Step {step} : {messages[step - 1]}
-      </div>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          // onMouseEnter={() => {
-          //   alert("TEST");
-          // }}
-          onClick={handlePrevious}
-          //OR
-          //onClick={()=>handlePrevious()}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    // added fragment "<>" instead of <div> so that parent div is not visible in the DOM, and <> behaves like a root element.
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen ? (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <div className="message">
+            Step {step} : {messages[step - 1]}
+          </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              // onMouseEnter={() => {
+              //   alert("TEST");
+              // }}
+              onClick={handlePrevious}
+              //OR
+              //onClick={()=>handlePrevious()}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 

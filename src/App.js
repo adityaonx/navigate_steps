bffd3329d92c9a6604cf412c0f7870f331e76018
adityaonx <adityaonx@gmail.com>
2023-11-13 12:20:45 +0530

    Added Button Component,Also used children Prop

--- src/App.js
+++ src/App.js
@@ -36,29 +36,28 @@ function App() {
             Step {step} : {messages[step - 1]}
           </div>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              // onMouseEnter={() => {
-              //   alert("TEST");
-              // }}
-              onClick={handlePrevious}
-              //OR
-              //onClick={()=>handlePrevious()}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>⬅️</span> Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next<span>➡️</span>
+            </Button>
           </div>
         </div>
       ) : (
         ""
       )}
     </>
+  );
+}
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
 
